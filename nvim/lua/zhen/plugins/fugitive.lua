87f8c7e1f6b2d075917a49ca026e2c800702c155
yamazhen <bowxyt@gmail.com>
2025-05-25 20:53:17 +0900

    test

--- nvim/lua/zhen/plugins/fugitive.lua
+++ nvim/lua/zhen/plugins/fugitive.lua
@@ -1,8 +1,19 @@
 return {
 	"tpope/vim-fugitive",
 	config = function()
-		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-		vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-		vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+		local keymap = vim.keymap.set
+		keymap("n", "<leader>gs", vim.cmd.Git)
+		keymap("n", "gu", "<cmd>diffget //2<CR>")
+		keymap("n", "gh", "<cmd>diffget //3<CR>")
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "fugitive",
+			callback = function()
+				keymap("n", "<leader>P", function()
+					-- let me try to use fugitive first
+					vim.cmd.Git({ "pull", "--rebase" })
+				end)
+			end,
+		})
 	end,
 }
