4fe44cecb9684c7635836792173a5853f4776ae2
yamazhen <bowxyt@gmail.com>
2025-08-07 22:15:20 +0900

    super minimal

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,6 @@
 vim.g.mapleader = " "
-vim.opt.guicursor = ""
-vim.o.winborder = "single"
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.cursorcolumn = false
 vim.opt.smartindent = true
 vim.opt.wrap = false
 vim.opt.ignorecase = true
@@ -17,16 +13,14 @@ vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
 
 vim.pack.add({
-    { src = "https://github.com/rose-pine/neovim",               name = "rose-pine" },
-    { src = "https://github.com/stevearc/oil.nvim" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-    { src = "https://github.com/ibhagwan/fzf-lua" },
-    { src = "https://github.com/mfussenegger/nvim-jdtls",        load = false },
-    { src = "https://github.com/neovim/nvim-lspconfig" },
-    { src = "https://github.com/echasnovski/mini.completion" },
-    { src = "https://github.com/mfussenegger/nvim-lint" },
-    { src = "https://github.com/tpope/vim-fugitive" },
-    { src = "https://github.com/folke/trouble.nvim" }
+	{ src = "https://github.com/rose-pine/neovim",               name = "rose-pine" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/ibhagwan/fzf-lua" },
+	{ src = "https://github.com/mfussenegger/nvim-jdtls",        load = false },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/echasnovski/mini.completion" },
+	{ src = "https://github.com/tpope/vim-fugitive" },
 })
 
 require("rose-pine").setup({ styles = { transparency = true } })
@@ -34,21 +28,33 @@ require("oil").setup({ view_options = { show_hidden = true } })
 require("fzf-lua").setup()
 require("mini.completion").setup()
 require("nvim-treesitter.configs").setup({ highlight = { enable = true }, auto_install = true })
-require("trouble").setup({ auto_preview = false, keys = { ["<cr>"] = "jump_close", } })
-require("config")
-require("autocmd")
 
 vim.lsp.enable({ "lua_ls", "ts_ls", "emmet_ls" })
 vim.opt.completeopt = "menu,menuone,noinsert"
+vim.cmd("colorscheme rose-pine-moon")
 
 local map = vim.keymap.set
 map("n", "<leader>o", ":update<CR> :source<CR>")
-map("n", "<leader>ee", ":update<CR> :Oil<CR>")
+map("n", "<leader>ee", ":Oil<CR>")
 map("n", "<C-e>", "<cmd>FzfLua files formatter='path.filename_first' previewer=false<CR>")
 map("n", "<C-p>", "<cmd>FzfLua git_files formatter='path.filename_first' previewer=false<CR>")
 map("n", "<leader>g", "<cmd>G<CR>")
-map("n", "<leader>tt", "<cmd>Trouble diagnostics toggle win = {type = float} focus=true filter.buf=0<cr>")
-map("n", "<leader>td", "<cmd>Trouble diagnostics toggle win = {type = float} focus=true<cr>")
+map("n", "<C-g>", "<cmd>G pull --rebase<CR>")
+map("n", "<leader>tt", vim.diagnostic.setloclist)
+map("n", "<leader>td", vim.diagnostic.setqflist)
 map({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
 map({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
 map({ 'n', 'v', 'x' }, '<leader>p', '"+p<CR>')
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd('FileType', {
+	pattern = 'qf',
+	callback = function() map('n', '<CR>', '<CR>:lclose<CR>:cclose<CR>', { buffer = true }) end
+})
+autocmd("BufWritePre", {
+	callback = function()
+		if #vim.lsp.get_clients({ bufnr = 0, method = "textDocument/formatting" }) > 0 then
+			vim.lsp.buf.format()
+		end
+	end
+})
