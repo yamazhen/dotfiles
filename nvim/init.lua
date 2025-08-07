f37d95dc65b10852f4487bfce4d78a924338cb5f
yamazhen <bowxyt@gmail.com>
2025-08-07 19:38:36 +0900

    trouble

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
-vim.opt.winborder = "single"
+vim.opt.guicursor = ""
+vim.o.winborder = "single"
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
@@ -13,6 +14,7 @@ vim.opt.signcolumn = "yes"
 vim.opt.termguicolors = true
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 10
 
 vim.pack.add({
     { src = "https://github.com/rose-pine/neovim",               name = "rose-pine" },
@@ -23,7 +25,8 @@ vim.pack.add({
     { src = "https://github.com/neovim/nvim-lspconfig" },
     { src = "https://github.com/echasnovski/mini.completion" },
     { src = "https://github.com/mfussenegger/nvim-lint" },
-    { src = "https://github.com/tpope/vim-fugitive" }
+    { src = "https://github.com/tpope/vim-fugitive" },
+    { src = "https://github.com/folke/trouble.nvim" }
 })
 
 require("rose-pine").setup({ styles = { transparency = true } })
@@ -31,6 +34,7 @@ require("oil").setup({ view_options = { show_hidden = true } })
 require("fzf-lua").setup()
 require("mini.completion").setup()
 require("nvim-treesitter.configs").setup({ highlight = { enable = true }, auto_install = true })
+require("trouble").setup({ auto_preview = false, keys = { ["<cr>"] = "jump_close", } })
 require("config")
 require("autocmd")
 
@@ -40,6 +44,8 @@ map("n", "<leader>ee", ":update<CR> :Oil<CR>")
 map("n", "<C-e>", "<cmd>FzfLua files formatter='path.filename_first' previewer=false<CR>")
 map("n", "<C-p>", "<cmd>FzfLua git_files formatter='path.filename_first' previewer=false<CR>")
 map("n", "<leader>g", "<cmd>G<CR>")
+map("n", "<leader>tt", "<cmd>Trouble diagnostics toggle win = {type = float} focus=true filter.buf=0<cr>")
+map("n", "<leader>td", "<cmd>Trouble diagnostics toggle win = {type = float} focus=true<cr>")
 map({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
 map({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
 map({ 'n', 'v', 'x' }, '<leader>p', '"+p<CR>')
---
+++ nvim/install_lsp.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+brew install lua-language-server
+brew install jdtls
+brew install eslint_d
