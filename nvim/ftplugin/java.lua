40c75286e1a124f2482f9fc07afb0ac8268f0e85
yamazhen <bowxyt@gmail.com>
2025-08-14 00:39:41 +0900

    proper lazy load + 50 line config

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,3 +1,5 @@
+vim.pack.add({ { src = "https://github.com/mfussenegger/nvim-jdtls" } })
+
 local jdtls = require("jdtls")
 
 local BREW_PREFIX = vim.fn.system("brew --prefix"):gsub("\n", "")
@@ -5,20 +7,20 @@ local CONFIG_PATH = vim.fn.stdpath("config")
 local WORKSPACE_BASE = os.getenv("HOME") .. "/jdtls/workspace"
 
 local function get_jdtls_paths()
-    local jdtls_path = BREW_PREFIX .. "/opt/jdtls"
-    return {
-        launcher = vim.fn.glob(jdtls_path .. "/libexec/plugins/org.eclipse.equinox.launcher_*.jar"),
-        config = jdtls_path .. "/libexec/config_mac",
-        lombok = BREW_PREFIX .. "/opt/lombok/libexec/lombok.jar",
-    }
+	local jdtls_path = BREW_PREFIX .. "/opt/jdtls"
+	return {
+		launcher = vim.fn.glob(jdtls_path .. "/libexec/plugins/org.eclipse.equinox.launcher_*.jar"),
+		config = jdtls_path .. "/libexec/config_mac",
+		lombok = BREW_PREFIX .. "/opt/lombok/libexec/lombok.jar",
+	}
 end
 
 local function get_workspace()
-    local root_dir = jdtls.setup.find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" })
-        or vim.fn.expand("%:p:h")
+	local root_dir = jdtls.setup.find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" })
+		or vim.fn.expand("%:p:h")
 
-    local project_name = vim.fn.fnamemodify(root_dir, ":p:h:t")
-    return WORKSPACE_BASE .. "/" .. project_name
+	local project_name = vim.fn.fnamemodify(root_dir, ":p:h:t")
+	return WORKSPACE_BASE .. "/" .. project_name
 end
 
 local paths = get_jdtls_paths()
@@ -26,103 +28,102 @@ local paths = get_jdtls_paths()
 local config = {}
 
 config.cmd = {
-    "java",
-    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-    "-Dosgi.bundles.defaultStartLevel=4",
-    "-Declipse.product=org.eclipse.jdt.ls.core.product",
-    "-Dlog.protocol=true",
-    "-Dlog.level=ALL",
-    "-Xmx1g",
-    "--add-modules=ALL-SYSTEM",
-    "--add-opens",
-    "java.base/java.util=ALL-UNNAMED",
-    "--add-opens",
-    "java.base/java.lang=ALL-UNNAMED",
-    "-javaagent:" .. paths.lombok,
-    "-jar",
-    paths.launcher,
-    "-configuration",
-    paths.config,
-    "-data",
-    get_workspace(),
+	"java",
+	"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+	"-Dosgi.bundles.defaultStartLevel=4",
+	"-Declipse.product=org.eclipse.jdt.ls.core.product",
+	"-Dlog.protocol=true",
+	"-Dlog.level=ALL",
+	"-Xmx1g",
+	"--add-modules=ALL-SYSTEM",
+	"--add-opens",
+	"java.base/java.util=ALL-UNNAMED",
+	"--add-opens",
+	"java.base/java.lang=ALL-UNNAMED",
+	"-javaagent:" .. paths.lombok,
+	"-jar",
+	paths.launcher,
+	"-configuration",
+	paths.config,
+	"-data",
+	get_workspace(),
 }
 
 config.settings = {
-    java = {
-        format = {
-            enabled = true,
-            settings = {
-                url = CONFIG_PATH .. "/lang_servers/intellij-java-google-style.xml",
-                profile = "GoogleStyle",
-            },
-        },
-        eclipse = {
-            downloadSource = true,
-        },
-        maven = {
-            downloadSources = true,
-        },
-        signatureHelp = {
-            enabled = true,
-        },
-        contentProvider = {
-            preferred = "fernflower",
-        },
-        saveActions = {
-            organizeImports = true,
-        },
-        completion = {
-            filteredTypes = {
-                "com.sun.*",
-                "io.micrometer.shaded.*",
-                "java.awt.*",
-                "jdk.*",
-                "sun.*",
-            },
-            importOrder = {
-                "java",
-                "jakarta",
-                "javax",
-                "com",
-                "org",
-            },
-        },
-        sources = {
-            organizeImports = {
-                starThreshold = 9999,
-                staticThreshold = 9999,
-            },
-        },
-        codeGeneration = {
-            toString = {
-                template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
-            },
-            hashCodeEquals = {
-                useJava7Objects = true,
-            },
-            useBlocks = true,
-        },
-        configuration = {
-            updateBuildConfiguration = "interactive",
-        },
-        referencesCodeLens = {
-            enabled = true,
-        },
-        inlayHints = {
-            parameterNames = {
-                enabled = "all",
-            },
-        },
-    },
+	java = {
+		format = {
+			enabled = true,
+			settings = {
+				url = CONFIG_PATH .. "/lang_servers/intellij-java-google-style.xml",
+				profile = "GoogleStyle",
+			},
+		},
+		eclipse = {
+			downloadSource = true,
+		},
+		maven = {
+			downloadSources = true,
+		},
+		signatureHelp = {
+			enabled = true,
+		},
+		contentProvider = {
+			preferred = "fernflower",
+		},
+		saveActions = {
+			organizeImports = true,
+		},
+		completion = {
+			filteredTypes = {
+				"com.sun.*",
+				"io.micrometer.shaded.*",
+				"java.awt.*",
+				"jdk.*",
+				"sun.*",
+			},
+			importOrder = {
+				"java",
+				"jakarta",
+				"javax",
+				"com",
+				"org",
+			},
+		},
+		sources = {
+			organizeImports = {
+				starThreshold = 9999,
+				staticThreshold = 9999,
+			},
+		},
+		codeGeneration = {
+			toString = {
+				template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+			},
+			hashCodeEquals = {
+				useJava7Objects = true,
+			},
+			useBlocks = true,
+		},
+		configuration = {
+			updateBuildConfiguration = "interactive",
+		},
+		referencesCodeLens = {
+			enabled = true,
+		},
+		inlayHints = {
+			parameterNames = {
+				enabled = "all",
+			},
+		},
+	},
 }
 
 config.capabilities = vim.lsp.protocol.make_client_capabilities()
 
 config.init_options = {
-    extendedClientCapabilities = vim.tbl_extend("force", jdtls.extendedClientCapabilities, {
-        resolveAdditionalTextEditsSupport = true,
-    }),
+	extendedClientCapabilities = vim.tbl_extend("force", jdtls.extendedClientCapabilities, {
+		resolveAdditionalTextEditsSupport = true,
+	}),
 }
 
-vim.cmd("packadd nvim-jdtls")
 require("jdtls").start_or_attach(config)
--- nvim/init.lua
+++ nvim/init.lua
@@ -18,7 +18,6 @@ vim.pack.add({
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/ibhagwan/fzf-lua" },
-	{ src = "https://github.com/mfussenegger/nvim-jdtls",        load = false },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/Saghen/blink.cmp" },
 	{ src = "https://github.com/rafamadriz/friendly-snippets" },
@@ -26,14 +25,14 @@ vim.pack.add({
 	{ src = "https://github.com/tpope/vim-fugitive" },
 })
 
+require("blink.cmp").setup({ snippets = { preset = "luasnip" } })
+require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_lua").load({ paths = { "~/.config/nvim/snippets/" } })
+require("luasnip").config.set_config({ region_check_events = "InsertEnter", delete_check_events = "InsertLeave" })
 require("rose-pine").setup({ styles = { transparency = true } })
 require("oil").setup({ view_options = { show_hidden = true } })
 require("fzf-lua").setup({ "ivy", winopts = { border = "none", preview = { hidden = true } } })
-require("blink.cmp").setup({ snippets = { preset = "luasnip" } })
 require("nvim-treesitter.configs").setup({ highlight = { enable = true }, auto_install = true })
-require("luasnip.loaders.from_lua").load({ paths = { "~/.config/nvim/snippets/" } })
-require("luasnip.loaders.from_vscode").lazy_load()
-require("luasnip").config.set_config({ region_check_events = "InsertEnter", delete_check_events = "InsertLeave" })
 
 vim.lsp.enable({ "lua_ls", "ts_ls", "emmet_ls", "html", "cssls", "kotlin_lsp" })
 vim.cmd("colorscheme rose-pine-moon")
@@ -46,14 +45,6 @@ vim.keymap.set("n", "<leader>g", ":G<CR>")
 vim.keymap.set("n", "<leader>si", vim.lsp.buf.code_action)
 vim.keymap.set("n", "<leader>tt", vim.diagnostic.setloclist)
 vim.keymap.set("n", "<leader>td", vim.diagnostic.setqflist)
+vim.keymap.set("n", "==", vim.lsp.buf.format)
 
-vim.api.nvim_create_autocmd('FileType',
-	{ pattern = 'qf', callback = function() vim.keymap.set('n', '<CR>', '<CR>:lclose<CR>:cclose<CR>', { buffer = true }) end })
-vim.api.nvim_create_autocmd("BufWritePre", {
-	callback = function()
-		if vim.bo.filetype == "html" and vim.fn.expand("%:e") == "ejs" then return end
-		if #vim.lsp.get_clients({ bufnr = 0, method = "textDocument/formatting" }) > 0 then
-			vim.lsp.buf.format()
-		end
-	end
-})
+vim.api.nvim_create_autocmd('FileType', { pattern = 'qf', callback = function() vim.keymap.set('n', '<CR>', '<CR>:lclose<CR>:cclose<CR>', { buffer = true }) end })
